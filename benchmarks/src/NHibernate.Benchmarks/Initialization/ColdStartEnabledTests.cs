d9a8f5324985c1902085691b27644f16ce095de5
Lillo <rafa_lillo@outlook.com>
2018-10-04 22:36:35 -0300

    Add more test

---
+++ benchmarks/src/NHibernate.Benchmarks/Initialization/ColdStartEnabledTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using NHibernate.Benchmarks.Models.AdventureWorks;
+
+namespace NHibernate.Benchmarks.Initialization
+{
+	public abstract class ColdStartEnabledTests : MarshalByRefObject
+	{
+		protected abstract ISession CreateSession();
+
+		public void CreateAndDisposeUnusedContext(int count)
+		{
+			for (var i = 0; i < count; i++)
+			{
+				// ReSharper disable once UnusedVariable
+				using (var session = CreateSession())
+				{
+				}
+			}
+		}
+
+		public void InitializeAndQuery_AdventureWorks(int count)
+		{
+			for (var i = 0; i < count; i++)
+			{
+				using (var session = CreateSession())
+				{
+					// ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+					session.Query<Department>().First();
+				}
+			}
+		}
+
+		public void InitializeAndSaveChanges_AdventureWorks(int count)
+		{
+			for (var i = 0; i < count; i++)
+			{
+				using (var session = CreateSession())
+				{
+					session.Save(
+						new Currency
+						{
+							CurrencyCode = "TMP",
+							Name = "Temporary"
+						});
+
+					using (session.BeginTransaction())
+					{
+						session.Flush();
+
+						// TODO: Don't mesure transaction rollback
+					}
+				}
+			}
+		}
+	}
+}
---
+++ benchmarks/src/NHibernate.Benchmarks/Mapping/AddressMap.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NHibernate.Benchmarks.Models.AdventureWorks;
+using NHibernate.Mapping.ByCode.Conformist;
+
+namespace NHibernate.Benchmarks.Mapping
+{
+	public class AddressMap : ClassMapping<Address>
+	{
+		public AddressMap()
+		{
+			Schema("Person");
+			Table("Address");
+
+
+			Property(x => x.AddressLine1, map => map.NotNullable(true));
+			/*
+			 *   entity.ToTable("Address", "Person");
+
+                        entity.Property(e => e.AddressLine1).IsRequired();
+
+                        entity.Property(e => e.City).IsRequired();
+
+                        entity.Property(e => e.ModifiedDate).HasDefaultValueSql("getdate()");
+
+                        entity.Property(e => e.PostalCode).IsRequired();
+
+                        entity.Property(e => e.rowguid).HasDefaultValueSql("newid()");
+
+                        entity.HasOne(d => d.StateProvince)
+                            .WithMany(p => p.Address)
+                            .HasForeignKey(d => d.StateProvinceID);
+			 */
+		}
+	}
+}
